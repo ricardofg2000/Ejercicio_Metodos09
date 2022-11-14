0ed830fdafd41070a4c255cd1f0ea96474d87d45
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-14 09:52:44 +0100

    Se añaden metodos para leer double y string

--- src/unico/Ejercicio00.java
+++ src/unico/Ejercicio00.java
@@ -8,23 +8,48 @@ public class Ejercicio00 {
 		
 		int edad;
 		int curso;
+		double peso;
+		String nombre;
 		
-		edad = leerDato("Escribe tu edad: ");
+		nombre = leerDatoString("¿Como te llamas? ");
+		edad = leerDatoInt("Escribe tu edad: ");
+		curso = leerDatoInt("¿Cuál es tu curso? ");
+		peso = leerDatoDouble("¿Cuanto pesas? ");
 		
-		curso = leerDato("¿Cuál es tu curso? ");
-		
+		System.out.println("Te llamas" + nombre);
 		System.out.println("Tienes " + edad + "años");
 		System.out.println("Cursas" + curso);
+		System.out.println("Tu peso es " + peso);
 		
 
 	}
 
-	 static int leerDato(String pregunta) {
+	 static int leerDatoInt(String pregunta) {
 		 Scanner leer = new Scanner(System.in);
 		 int respuesta;
 		 
 		 System.out.print(pregunta);
 		 respuesta = Integer.parseInt(leer.nextLine());
+		 
+		 return respuesta;
+	}
+	 
+	 static double leerDatoDouble(String pregunta) {
+		 Scanner leer = new Scanner(System.in);
+		 double respuesta;
+		 
+		 System.out.print(pregunta);
+		 respuesta = Double.parseDouble(leer.nextLine());
+		 
+		 return respuesta;
+	}
+	 
+	 static String leerDatoString(String pregunta) {
+		 Scanner leer = new Scanner(System.in);
+		 String respuesta;
+		 
+		 System.out.print(pregunta);
+		 respuesta = leer.nextLine();
 		 
 		 return respuesta;
 	}
